6fd2e9336c4016dc3b2475f174bec5314632b3fb
Jacqueline Li <jackieli878@gmail.com>
2023-10-20 14:57:28 -0400

    implement functionality

---
+++ module9-solution/src/routes.js
@@ -0,0 +1,48 @@
+(function () {
+    'use strict';
+
+    angular.module('MenuApp')
+        .config(RoutesConfig);
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+        // Redirect to home page if no other URL matches
+        $urlRouterProvider.otherwise('/');
+
+        // *** Set up UI states ***
+        $stateProvider
+
+            // Home page
+            .state('home', {
+                url: '/',
+                templateUrl: 'src/menuApp/templates/home.template.html'
+            })
+
+            // Categories page
+            .state('categories', {
+                url: '/categories',
+                templateUrl: 'src/menuapp/templates/main-categories.template.html',
+                controller: 'CategoriesController as categories',
+                resolve: {
+                    items: ['MenuDataService', function (MenuDataService) {
+                        return MenuDataService.getAllCategories();
+                    }]
+                }
+            })
+
+            // Items page
+            .state('items', {
+                url: '/items/{categoryShortName}',
+                templateUrl: 'src/menuapp/templates/main-items.template.html',
+                controller: 'ItemsController as itemsForCategory',
+                resolve: {
+                    items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+                        return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+                    }]
+                }
+            });
+
+    }
+
+})();
