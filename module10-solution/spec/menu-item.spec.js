f854f765c983a39b0984d144d2c4125c7538cf4c
Jacqueline Li <jackieli878@gmail.com>
2023-11-06 22:17:56 -0500

    add test

--- module10-solution/spec/menu-item.spec.js
+++ module10-solution/spec/menu-item.spec.js
@@ -14,4 +14,26 @@ describe('menuService', function () {
     });
   });
 
+  it('should return menu item with short name L1', function () {
+    $httpBackend.whenGET(ApiPath + '/menu_items/L/menu_items/0.json').respond(
+      {
+        "description": "chunks of chicken, breaded and deep-fried with sauce containing orange peels; white meat by request: for pint $1 extra, for large $2 extra",
+        "name": "Orange Chicken",
+        "price_large": 9.75,
+        "short_name": "L1"
+      }
+    );
+    menuService.getMenuItem('L', '0').then(function (response) {
+      expect(response).toEqual(
+        {
+          "description": "chunks of chicken, breaded and deep-fried with sauce containing orange peels; white meat by request: for pint $1 extra, for large $2 extra",
+          "name": "Orange Chicken",
+          "price_large": 9.75,
+          "short_name": "L1"
+        }
+      );
+    });
+    $httpBackend.flush();
+  });
+  
 });
